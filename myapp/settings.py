d231c9bc9daee5c1a6e5e7c8b99680f0a6eb9eaa
Md. Tushar Ahmed <99356217+tushar-3549@users.noreply.github.com>
2025-04-25 15:56:08 +0600

    Update settings.py

--- myapp/settings.py
+++ myapp/settings.py
@@ -26,7 +26,11 @@ SECRET_KEY = 'django-insecure-yf33w5im19!$rt9#x-+cjh&dzv9$0fd+(l$yaev%3+9$_1t%94
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+# ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["tweet-django-bkgn.onrender.com", "localhost", "127.0.0.1"]
+CSRF_TRUSTED_ORIGINS = [
+    "https://tweet-django-bkgn.onrender.com",
+]
 
 
 # Application definition
